bf1e49093dc6dfd12789f6de3fae9b31952cf01c
EUye9IM <euye9im@euye9im-yoga14s.(none)>
2022-06-12 23:21:44 +0800

    添加了DER格式的证书

--- sign/sign.sh
+++ sign/sign.sh
@@ -3,8 +3,10 @@ HOST=127.0.0.1
 
 openssl req -new \
 	-newkey rsa:2048 \
-	-keyout server.key -out server.csr \
+	-keyout server.pem.key -out server.pem.csr \
 	-nodes -x509 \
 	-days 3650 \
 	-subj "/C=CN/ST=Beijing/L=Beijing/O=Me/OU=Me/CN=me.org" \
 	-addext "subjectAltName = IP:$HOST"
+
+openssl x509 -outform der -in server.pem.csr -out certificate.der.cer
